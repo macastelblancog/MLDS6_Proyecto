db3efd2f124e282cd2820f1138e9460b6150deaf
Miguel Castelblanco <64758713+macastelblancog@users.noreply.github.com>
2024-05-16 20:01:49 -0500

    Estructura libreria
    Se modifico la carpeta de origen de los datos para crear el modelo y se creo un modulo llamado bazinga para todas las rutinas asociadas al proyectp.

--- docs/data/utils/.ipynb_checkpoints/loader-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_raw_data(path : str = None):
-    if path is None:
-        path = '../inputs/Sarcasm_Headlines_Dataset.json'
-    try:
-        return pd.read_json(path, lines=True)
-    except:
-        raise FileNotFoundError('Error en archivo de datos.')
